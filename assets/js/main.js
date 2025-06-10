7e502d8c9857ebbbccf077e8f3659ace278dc4c7
Lucia994 <l.summa94@gmail.com>
2025-06-10 15:44:00 +0200

    Setup initial Javscript project structure

---
+++ assets/js/main.js
@@ -0,0 +1,100 @@
+console.log("Hello");
+// Snack 1
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+// Snack2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// Snack 1
+// Creare un array di 10 oggetti(racingBike) con le proprietà: nome e peso. 
+const racingBike = [
+    {
+        bikeName: "Bianchi",
+        bikeweight: 6.8,
+ },
+{
+        bikeName: "Look",
+        bikeweight: 8,
+ },
+ {
+        bikeName: "Willier Triestina",
+        bikeweight: 7.5, 
+ },
+ {
+        bikeName: "Pinarello",
+        bikeweight: 6.9, 
+ },
+ {
+        bikeName: "Colnago",
+        bikeweight: 7.3,
+ },
+ {
+        bikeName: "Cervelo S5",
+        bikeweight: 6.7,
+ },
+ {
+        bikeName: "Canyon",
+        bikeweight: 7.9,
+ },
+ {
+        bikeName: "Merida Reacto Team",
+        bikeweight: 7.4,
+ },
+ {
+        bikeName: "Specialized",
+        bikeweight: 6.6, 
+ },
+ {
+        bikeName: "Trek",
+        bikeweight: 7.7,
+ } 
+];
+
+// Stampare a schermo la bici con peso minore.
+
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. 
+
+const footballTeams = [
+    { namefootballTeams: "Real Madrid",
+        pointsScored: 0,
+        foulsDrawn: 0, 
+
+},
+{ namefootballTeams: "Barcellona",
+        pointsScored: 0,
+        foulsDrawn: 0,
+
+},
+{ namefootballTeams: "Liverpool",
+        pointsScored: 0,
+        foulsDrawn: 0,
+
+},
+{ namefootballTeams: "Inter",
+        pointsScored: 0, 
+        foulsDrawn: 0,
+
+},
+{ namefootballTeams: "Juventus",
+        pointsScored: 0,
+        foulsDrawn: 0,
+
+},
+{ namefootballTeams: "Roma",
+        pointsScored: 0,
+        foulsDrawn: 0,
+
+},
+{ namefootballTeams: "Manchester United",
+        pointsScored: 0,
+        foulsDrawn: 0,
+
+},
+{ namefootballTeams: "Sampdoria",
+        pointsScored: 0,
+        foulsDrawn: 0,
+
+}
+]; 
